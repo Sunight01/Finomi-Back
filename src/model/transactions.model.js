e3610d4363e8691958b9bea0b876ec3ab7a0abde
Sunight01 <kevin.bmorillas@gmail.com>
2024-08-29 13:25:20 -0400

    feat(fix): Fixed amount in transactions

--- src/model/transactions.model.js
+++ src/model/transactions.model.js
@@ -1,6 +1,14 @@
 import DB from "../database.js";
 
-const insertTransaction = async (user_id, title, type, date, tag, description, amount) => {
+const insertTransaction = async (
+  user_id,
+  title,
+  type,
+  date,
+  tag,
+  description,
+  amount
+) => {
   const query = `
   INSERT INTO transactions (user_id, title, type, date, tag, description, amount)
   VALUES ($1, $2, $3, $4, $5, $6, $7)
@@ -14,7 +22,7 @@ const insertTransaction = async (user_id, title, type, date, tag, description, a
     date,
     tag,
     description,
-    amount
+    amount,
   ]);
   return JSON.parse(JSON.stringify(rows[0]));
 };
@@ -23,14 +31,12 @@ const getTransactions = async (user_id) => {
   const query = `
     SELECT * FROM transactions
     WHERE user_id = $1
-  `
+  `;
 
   const client = DB.getClient();
-  const { rows } = await client.query(query, [
-    user_id
-  ]);
-  return JSON.parse(JSON.stringify(rows));;
-}
+  const { rows } = await client.query(query, [user_id]);
+  return JSON.parse(JSON.stringify(rows));
+};
 
 export default {
   insertTransaction,
